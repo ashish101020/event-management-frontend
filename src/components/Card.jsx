112a5607eb4c39d12770c655216e92a33ee2b95c
ashish <guptashish101020@gmail.com>
2026-02-09 22:28:48 +0530

    title color change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ const Card = (event) => {
   return (
     <div style={{margin:"5px", padding:"5px", width:"200px", height:"200px", borderRadius:"8px"}}>
         <img src="" alt="" />
-        <p>{event.title}</p>
+        <p style={{color:"yellowgreen"}}>{event.title}</p>
         <p>{event.description}</p>
         <p>{event.location}</p>
         <p>{event.status}</p>
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,20 +5,15 @@ const Events = () => {
   const { events } = useEventContext();
 
   return (
-    <div style={{ padding: "10px", display: "flex", gap: "6px", flexWrap: "wrap", color:"yellowgreen" }}>
+    <div style={{ padding: "10px", display: "flex", gap: "6px", flexWrap: "wrap", color:"white" }}>
 
-       <div>
-      {/* {events && events.length > 0 ? (
+      {events && events.length > 0 ? (
         events.map((event) => (
           <Card event={event} key={event._id} />
         ))
       ) : (
         <div style={{display:"flex", justifyContent:"center", alignContent:"center", fontSize:"72px", fontWeight:"bold", color:"white"}}>No events found</div>
-      )} */}
-        <h3>updated test event 123</h3>
-        <p>Ranchi</p>
-        <p>Upcoming</p>
-      </div>
+      )}
     </div>
   );
 };
