2f8aac40cb928084064df774f1438436e663fee2
ashish <guptashish101020@gmail.com>
2026-02-09 18:18:01 +0530

    a

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -6,13 +6,18 @@ const Events = () => {
 
   return (
     <div style={{ padding: "10px", display: "flex", gap: "6px", flexWrap: "wrap" }}>
-      {events && events.length > 0 ? (
+      {/* {events && events.length > 0 ? (
         events.map((event) => (
           <Card event={event} key={event._id} />
         ))
       ) : (
         <div style={{display:"flex", justifyContent:"center", alignContent:"center", fontSize:"72px", fontWeight:"bold", color:"white"}}>No events found</div>
-      )}
+      )} */}
+      <div>
+        <h3>updated test event 123</h3>
+        <p>Ranchi</p>
+        <p>Upcoming</p>
+      </div>
     </div>
   );
 };
